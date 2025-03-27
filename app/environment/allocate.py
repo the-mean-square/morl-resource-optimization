039ab077e33acd1f1e43797ebabd94294347aeeb
Ranjith <ranjith2901@gmail.com>
2025-03-27 16:41:57 +0530

    allocate.py

---
+++ app/environment/allocate.py
@@ -0,0 +1,8 @@
+""" Airport class: Custom environment inheriting from gym.Env
+- The base class for environments in the gymnasium (formerly open AI Gym) framework """
+
+import pandas as pd
+import numpy as np
+
+class AirportEnv(gym.Env):
+    pass
